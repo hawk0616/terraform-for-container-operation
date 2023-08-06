a526fe58696ce2a458e10a9d762e82321a56dbba
遠藤大志 <endoutaishi@endouhiroshinoMacBook-Pro.local>
2023-08-07 01:06:49 +0900

    delete resources

--- ecr.tf
+++ ecr.tf
@@ -1,34 +1,34 @@
-resource "aws_ecr_repository" "ecr_repository" {
-  name = local.app
-  # イメージタグの上書きを禁止
-  image_tag_mutability = "IMMUTABLE"
-  force_delete         = true
+# resource "aws_ecr_repository" "ecr_repository" {
+#   name = local.app
+#   # イメージタグの上書きを禁止
+#   image_tag_mutability = "IMMUTABLE"
+#   force_delete         = true
 
-  image_scanning_configuration {
-    # プッシュされたイメージをスキャニングして脆弱性を検出
-    scan_on_push = true
-  }
-}
+#   image_scanning_configuration {
+#     # プッシュされたイメージをスキャニングして脆弱性を検出
+#     scan_on_push = true
+#   }
+# }
 
-resource "aws_ecr_lifecycle_policy" "ecr_lifecycle_policy" {
-  repository = aws_ecr_repository.ecr_repository.name
+# resource "aws_ecr_lifecycle_policy" "ecr_lifecycle_policy" {
+#   repository = aws_ecr_repository.ecr_repository.name
 
-  policy = <<EOF
-{
-    "rules": [
-        {
-            "rulePriority": 1,
-            "description": "Keep last 30 images",
-            "selection": {
-                "tagStatus": "any",
-                "countType": "imageCountMoreThan",
-                "countNumber": 30
-            },
-            "action": {
-                "type": "expire"
-            }
-        }
-    ]
-}
-EOF
-}
+#   policy = <<EOF
+# {
+#     "rules": [
+#         {
+#             "rulePriority": 1,
+#             "description": "Keep last 30 images",
+#             "selection": {
+#                 "tagStatus": "any",
+#                 "countType": "imageCountMoreThan",
+#                 "countNumber": 30
+#             },
+#             "action": {
+#                 "type": "expire"
+#             }
+#         }
+#     ]
+# }
+# EOF
+# }
--- iam.tf
+++ iam.tf
@@ -1,38 +1,38 @@
-data "aws_iam_policy_document" "ecs_task_assume" {
-  # ロール引き受けの権限を付与
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
+# data "aws_iam_policy_document" "ecs_task_assume" {
+#   # ロール引き受けの権限を付与
+#   statement {
+#     actions = ["sts:AssumeRole"]
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ecs-tasks.amazonaws.com"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "ecs_task" {
-  name               = "${local.app}-ecs-task"
-  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume.json
-}
+# resource "aws_iam_role" "ecs_task" {
+#   name               = "${local.app}-ecs-task"
+#   assume_role_policy = data.aws_iam_policy_document.ecs_task_assume.json
+# }
 
-# タスク実行ロール用のIAMロールを作成
-data "aws_iam_policy_document" "ecs_assume" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ecs-tasks.amazonaws.com"]
-    }
-  }
-}
+# # タスク実行ロール用のIAMロールを作成
+# data "aws_iam_policy_document" "ecs_assume" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ecs-tasks.amazonaws.com"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "ecs" {
-  name               = "${local.app}-ecs"
-  assume_role_policy = data.aws_iam_policy_document.ecs_assume.json
-}
+# resource "aws_iam_role" "ecs" {
+#   name               = "${local.app}-ecs"
+#   assume_role_policy = data.aws_iam_policy_document.ecs_assume.json
+# }
 
-# コンテナイメージをECRからgetしてpull
-# CloudwatchLogsにログお出力先を作成し、ログを出力する
-resource "aws_iam_role_policy_attachment" "ecs_basic" {
-  role       = aws_iam_role.ecs.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
+# # コンテナイメージをECRからgetしてpull
+# # CloudwatchLogsにログお出力先を作成し、ログを出力する
+# resource "aws_iam_role_policy_attachment" "ecs_basic" {
+#   role       = aws_iam_role.ecs.name
+#   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# }
